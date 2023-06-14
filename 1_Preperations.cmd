2dd7d3b41a5e66475bb6559293d069e1b4a98502
JG <it@hgw-tornesch.de>
2023-06-14 14:38:28 +0200

    Changed names & added disabling Cortana to start menu search disabler

--- 1_Preperations.cmd
+++ 1_Preperations.cmd
@@ -4,9 +4,9 @@ robocopy "ScriptFiles" "%localappdata%\Microsoft\Windows\Shell" "LayoutModificat
 robocopy "ScriptFiles" "C:\Install" "unattend.xml"
 robocopy "ScriptFiles" "C:\ProgramData\Install" "DefaultAssociations.xml"
 
-reg import "ScriptFiles\Deactivate AeroShake.reg"
-reg import "ScriptFiles\Deactivate Start Menu Web Search.reg"
-reg import "ScriptFiles\Deactivate News & Meet.reg"
+reg import "ScriptFiles\Disable AeroShake.reg"
+reg import "ScriptFiles\Disable Cortana and Start Menu Web Search.reg"
+reg import "ScriptFiles\Disable News & Meet.reg"
 reg import "ScriptFiles\Set Default Apps.reg"
 
 popd
