7b50cff3befc8aa865bd870d9ac528a6b5a9995d
Rsge <rsge@web.de>
2023-07-31 15:42:31 +0200

    Added disabling the 3D Objects folder

--- 1_Preperations.cmd
+++ 1_Preperations.cmd
@@ -4,6 +4,7 @@ robocopy "ScriptFiles" "%localappdata%\Microsoft\Windows\Shell" "LayoutModificat
 robocopy "ScriptFiles" "C:\Install" "unattend.xml"
 robocopy "ScriptFiles" "C:\ProgramData\Install" "DefaultAssociations.xml"
 
+reg import "ScriptFiles\Disable 3D Objects folder.reg"
 reg import "ScriptFiles\Disable AeroShake.reg"
 reg import "ScriptFiles\Disable Cortana and Start Menu Web Search.reg"
 reg import "ScriptFiles\Disable News & Meet.reg"
